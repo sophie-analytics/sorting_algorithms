8c0384e0d85c5385e8603dcb90a78b404d150c3a
Sofiat Sonibare <sonibaresofiat@gmail.com>
2024-01-29 11:09:14 +0100

    added an extra condition

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,21 +7,22 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i;
-    size_t j;
-    size_t temp;
+size_t i, j, temp;
 
-    for (i = 0; i < size - 1; i++)
+if (array == NULL || size < 2)
+    return;
+
+for (i = 0; i < size - 1; i++)
+{
+    for (j = 0; j < size - i - 1; j++)
     {
-        for (j = 0; j < size - i - 1; j++)
+        if (array[j] > array[j + 1])
         {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-            }
+            temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+            print_array(array, size);
         }
     }
+}
 }
